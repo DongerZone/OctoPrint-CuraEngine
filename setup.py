4e46dcddb1badc476f80b31a91dc33d4c920d90a
Kieran Ramnarine <kieran.ramnarine18@gmail.com>
2019-01-31 14:11:45 -0500

    Update setup.py
    Updated new information

--- setup.py
+++ setup.py
@@ -18,16 +18,16 @@ plugin_version = "0.1.0"
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
-plugin_description = """TODO"""
+plugin_description = "Uses the Cura 2.x Enging to slice STL files with the parameters entered"
 
 # The plugin's author. Can be overwritten within OctoPrint's internal data via __plugin_author__ in the plugin module
-plugin_author = "Nicanor Romero Venier"
+plugin_author = "Kieran Ramnarine"
 
 # The plugin's author's mail address.
-plugin_author_email = "nicanor.romerovenier@bq.com"
+plugin_author_email = "kieran.ramnarine18@gmail.com"
 
 # The plugin's homepage URL. Can be overwritten within OctoPrint's internal data via __plugin_url__ in the plugin module
-plugin_url = "https://github.com/nicanor-romero/OctoPrint-CuraEngine"
+plugin_url = "https://github.com/DongerZone/OctoPrint-CuraEngine"
 
 # The plugin's license. Can be overwritten within OctoPrint's internal data via __plugin_license__ in the plugin module
 plugin_license = "AGPLv3"
